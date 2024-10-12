495ae6ee5fa2229d74a10db2a656417b663370a8
Jaime Ignacio <jaime.ignaciodev@gmail.com>
2024-10-12 16:46:25 -0300

    Avances de traducción de los elementos

---
+++ content/themes/default/templates_compiled/dccd25b92ac8435defb3ef8afef46a80f278779a_0.file_sign.tpl.php
@@ -0,0 +1,48 @@
+<?php
+/* Smarty version 5.4.1, created on 2024-10-12 19:08:51
+  from 'file:sign.tpl' */
+
+/* @var \Smarty\Template $_smarty_tpl */
+if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
+  'version' => '5.4.1',
+  'unifunc' => 'content_670ac943acf159_49531336',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'dccd25b92ac8435defb3ef8afef46a80f278779a' => 
+    array (
+      0 => 'sign.tpl',
+      1 => 1710768843,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:_head.tpl' => 1,
+    'file:_header.tpl' => 1,
+    'file:_sign_form.tpl' => 1,
+    'file:_footer.tpl' => 1,
+  ),
+))) {
+function content_670ac943acf159_49531336 (\Smarty\Template $_smarty_tpl) {
+$_smarty_current_dir = 'C:\\xampp\\htdocs\\HospitalRegional\\content\\themes\\default\\templates';
+$_smarty_tpl->renderSubTemplate('file:_head.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), (int) 0, $_smarty_current_dir);
+$_smarty_tpl->renderSubTemplate('file:_header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), (int) 0, $_smarty_current_dir);
+?>
+
+<!-- page content -->
+<div class="container mt30">
+  <div class="row">
+    <div class="col-md-6 col-lg-5 mx-md-auto">
+      <!-- sign in/up form -->
+      <?php $_smarty_tpl->renderSubTemplate('file:_sign_form.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), (int) 0, $_smarty_current_dir);
+?>
+      <!-- sign in/up form -->
+    </div>
+  </div>
+</div>
+<!-- page content -->
+
+<?php $_smarty_tpl->renderSubTemplate('file:_footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), (int) 0, $_smarty_current_dir);
+}
+}
